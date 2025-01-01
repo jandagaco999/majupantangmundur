10f5c9a47f05b60f68efdc19420f07e3159c7689
jandagaco999 <jandagaco999@gmail.com>
2025-01-02 01:10:59 +0700

    Create backlink.php

---
+++ backlink.php
@@ -0,0 +1,7 @@
+<div style="display:none">
+<a href="https://jdih.pn-koba.go.id/">https://jdih.pn-koba.go.id/</a>
+<a href="https://penerbit.umk.ac.id/janda/">https://penerbit.umk.ac.id/janda/</a>
+<a href="https://vote.smkn1sikur.sch.id/">https://vote.smkn1sikur.sch.id/</a>
+<a href="https://eemisas.aknpacitan.ac.id/dpr/">https://eemisas.aknpacitan.ac.id/dpr/</a>
+<a href="https://peru.marssociety.org/vendor/">https://peru.marssociety.org/vendor/</a>
+</div>
